1f08741ad5e8393b191d71cc72c204e0dfb7d34e
Sultan Faaiz <sultan.faaiz@yahoo.com>
2025-09-17 08:58:48 +0700

    feat: Configure allowed development origins in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    allowedDevOrigins: ['192.168.1.21', 'local-origin.dev', '*.local-origin.dev'],
+};
 
 export default withFlowbiteReact(nextConfig);
