f47cd88d1fdc1e2828ff7d8f0988459db296df29
Sultan Faaiz <sultan.faaiz@yahoo.com>
2025-09-19 15:14:22 +0700

    fix: Remove allowedDevOrigins and clean up nextConfig structure

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    allowedDevOrigins: ['192.168.1.21', 'local-origin.dev', '*.local-origin.dev'],
-    basePath : '/nichiha',
-    assetPrefix: "/nichiha/",
+  reactStrictMode: true,
+  basePath: "/nichiha",
+  // assetPrefix: "/nichiha/", // ⚠️ disable dulu
 };
 
 export default withFlowbiteReact(nextConfig);
