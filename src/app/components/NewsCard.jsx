c2ef21ad7ea4e878d180e13e8b76869a91c80e08
Sultan Faaiz <sultan.faaiz@yahoo.com>
2025-09-24 11:32:58 +0700

    fix: update category text color for improved readability in NewsCard component

--- src/app/components/NewsCard.jsx
+++ src/app/components/NewsCard.jsx
@@ -16,7 +16,7 @@ export default function NewsCard({ category, title, image, link }) {
       <div className="relative -mt-20 sm:-mt-24 z-10 sm:px-6 md:px-5">
         <div className="p-4 sm:p-5 flex flex-col justify-between gap-3 w-[70%] sm:w-[90%] md:w-[80%] h-[320px] sm:h-[360px] md:h-[400px] bg-white mx-auto rounded-xl shadow-lg">
           <div>
-            <span className="text-gray-400 font-semibold text-base lg:text-lg">
+            <span className="text-gray-500 font-semibold text-base lg:text-lg">
               {category}
             </span>
             <h3 className="text-xl lg:text-2xl font-bold text-gray-800 leading-snug mt-1">
