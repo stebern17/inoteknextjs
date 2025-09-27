d40b2513fa3f1dc429b6b0bed1982c5379ba2773
Sultan Faaiz <sultan.faaiz@yahoo.com>
2025-09-27 08:49:21 +0700

    feat: Replace video files with webm format and update video source based on screen size

--- src/app/nichiha/VideoSection.jsx
+++ src/app/nichiha/VideoSection.jsx
@@ -1,13 +1,30 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function VideoSection() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1024); // Adjust the breakpoint as needed
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Check initial size
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section className="flex justify-center items-center">
-      {/* note : mobile bisa vh full dengan syarat ada video resolusi mobile */}
+      {/* change the video source based on screen size */}
       <video
-        src="/videos/VideoProfile.mp4"
+        src={
+          isMobile
+            ? "/videos/VideoProfilePotrait.webm"
+            : "/videos/VideoProfile.webm"
+        }
         controls
         className="min-h-screen object-cover hidden lg:block"
         controlsList="nodownload noremoteplayback"
@@ -17,18 +34,7 @@ function VideoSection() {
         playsInline
         onContextMenu={(e) => e.preventDefault()}
         preload="auto"
-      ></video>
-      <video
-        src="/videos/VideoProfilePotrait.mp4"
-        controls
-        className="min-h-screen object-cover lg:hidden"
-        controlsList="nodownload noremoteplayback"
-        autoPlay
-        muted
-        loop
-        playsInline
-        onContextMenu={(e) => e.preventDefault()}
-        preload="auto"
+        poster="/images/company profile-1.jpg"
       ></video>
     </section>
   );
