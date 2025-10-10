31a8857016dffe13fd8ab023c447d74ae98428c4
Sultan Faaiz <sultan.faaiz@yahoo.com>
2025-10-10 13:57:22 +0700

    feat: update CardCatalogGird layout to improve responsiveness with adjusted grid columns

--- src/app/catalog/CardCatalogGird.jsx
+++ src/app/catalog/CardCatalogGird.jsx
@@ -4,7 +4,7 @@ export default async function CatalogPage() {
   const catalogs = await getCatalogData();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 p-6">
       {catalogs.map((catalog) => (
         <a
           href={catalog.catalogFile}
