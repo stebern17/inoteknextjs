1163ebe805e7f0da3ca3a4f48d62e3f609e5cd04
Sultan Faaiz <sultan.faaiz@yahoo.com>
2025-12-30 18:50:28 +0700

    feat: replace NichihaPage with individual sections in Home component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,21 @@
-import NichihaPage from "./nichiha/page";
+import VideoSection from "./nichiha/VideoSection";
+import HeroSection from "./nichiha/HeroSection";
+import UspSection from "./nichiha/UspSection";
+import ProductSection from "./nichiha/ProductSection";
+import NewsSection from "./nichiha/NewsSection";
+import DistributionSection from "./nichiha/DistributionSection";
 
 export default function Home() {
-  return <NichihaPage />;
+  return (
+    <>
+      <VideoSection />
+      <HeroSection />
+      <UspSection />
+      <div>
+        <ProductSection />
+        <NewsSection />
+      </div>
+      <DistributionSection />
+    </>
+  );
 }
